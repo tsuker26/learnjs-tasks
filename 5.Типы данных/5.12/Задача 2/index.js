efe0cfe1a45f95e4e2af1ce8a3721d55a17ff61b
Igor Tsuker <igorek888rane@gmail.com>
2023-02-25 12:19:27 +0300

    5.Типы данных

---
+++ 5.Типы данных/5.12/Задача 2/index.js
@@ -0,0 +1,16 @@
+let room = {
+    number: 23
+};
+
+let meetup = {
+    title: "Совещание",
+    occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+    place: room
+};
+
+// цикличные ссылки
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+console.log( JSON.stringify(meetup, (key, value) =>  (key !== '' && value === meetup) ? undefined : value));
+
